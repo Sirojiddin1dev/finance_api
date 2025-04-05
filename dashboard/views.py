30c63da9740374fec397ed3432aff7e5e784f257
Abdullox <your-email@example.com>
2025-04-05 17:48:52 +0300

    Abdullox

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from main.models import Notification, About , Video , Help, UserProfile
+from main.serializers import NotificationSerializer, AboutSerializer, VideoSerializer, HelpSerializer, UserProfileSerializer
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+class NotificationView(ListAPIView):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+
+class AboutView(ListAPIView):
+    queryset = About.objects.all()
+    serializer_class = AboutSerializer
+
+
+class VideoView(ListAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+
+class HelpView(ListAPIView):
+    queryset = Help.objects.all()
+    serializer_class = HelpSerializer
+
+
+class UserProfileView(ListAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
